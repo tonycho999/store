f3dc4323cdc140a0cf2c5b5d5a46bd5ad78f6afd
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-01-21 20:05:49 +0800

    sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'sweet-cakes-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './db.js',
+    './manifest.json',
+    'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
